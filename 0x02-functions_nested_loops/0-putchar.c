e16df4ec0f5898fd63e5ed7d82f50a5283ac78ed
yahya <yahyasahnoun74@gmail.com>
2023-03-28 11:27:05 -0700

    task done

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/*
+ *
+ *
+ */
+int main(void) {
+       char str[] = "_putchar" ;
+       int ch ;
+       for (ch=0 ;ch < 8 ;ch++)
+	      _putchar(str[]);
+      _putchar('\n') ;
+      return(0);
+}
